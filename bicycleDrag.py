679acaf0a1451872443ef3313db2aa08179ed144
Cronographer <djisenberg2015@gmail.com>
2020-02-06 12:16:38 -0500

    Adjusted bicycleDrag.py to now run and plot the problem once for each value in "mass table", all on the same graph. This version still has the print statements I used to debug that process in it. Also added bicycleLossless.py to this committ, because it works as is and it wasn't committed before. The error complaining about "no module named matplotlib" is still there for some reason, and it is still clearly incorrect. 3 warnings are not causing any problems, I don't quite understand what it is trying to tell me right now, will look into it if it persists/becomes an issue.

--- bicycleDrag.py
+++ bicycleDrag.py
@@ -13,7 +13,7 @@ Unless otherwise noted all values are expressed in standard SI units
 """
 import matplotlib.pyplot as plt
 
-mass = 70.0
+
 powerOutput = 400.0
 dragCoefficient = 0.5
 crossSectionArea = 0.33
@@ -25,30 +25,57 @@ timeStep = 0.1
 maxTime = 200.0
 initialTime = 0.0
 currentTime = initialTime
+tableIndex = 0
 
 
 timeDataList = []
 losslessVelocityDataList = []
 dragVelocityDataList = []
+massTable = [65, 70, 75]
+lableTable = ["mass = 65kg", "mass = 70kg", "mass = 75kg"]
 
 timeDataList.append(initialTime)
 losslessVelocityDataList.append(losslessVelocity)
 dragVelocityDataList.append(dragVelocity)
+print("line(41)")
+print("len(massTable) is: " + str(len(massTable)))
+while tableIndex <= len(massTable) - 1:
+    print("")
+    print("line 44")
+    print("table Index is: " + str(tableIndex))
+    print("massTable[tableIndex] is: " + str(massTable[tableIndex]))
+    while currentTime <= maxTime:
+        mass = massTable[tableIndex]
+        losslessVelocity = losslessVelocity + (powerOutput / (mass * losslessVelocity)) * timeStep
+        dragVelocity = dragVelocity + ((powerOutput / (mass * dragVelocity)) - ((dragCoefficient * airDensity * crossSectionArea * dragVelocity**2)/(2*mass))) * timeStep
 
-while currentTime <= maxTime:
-    losslessVelocity = losslessVelocity + (powerOutput / (mass * losslessVelocity)) * timeStep
-    dragVelocity = dragVelocity + ((powerOutput / (mass * dragVelocity)) - ((dragCoefficient * airDensity * crossSectionArea * dragVelocity**2)/(2*mass))) * timeStep
+        currentTime = currentTime + timeStep
+        timeDataList.append(currentTime)
+        losslessVelocityDataList.append(losslessVelocity)
+        dragVelocityDataList.append(dragVelocity)
+        #print(currentTime, losslessVelocity, dragVelocity)
 
-    currentTime = currentTime + timeStep
-    timeDataList.append(currentTime)
-    losslessVelocityDataList.append(losslessVelocity)
-    dragVelocityDataList.append(dragVelocity)
-    #print(currentTime, losslessVelocity, dragVelocity)
-    print("\n")
+    plt.plot(timeDataList, losslessVelocityDataList, label="without drag")
+    plt.plot(timeDataList, dragVelocityDataList, label=lableTable[tableIndex])
+
+    print("")
+    print("line 62")
+    print("")
+    print("table Index is: " + str(tableIndex))
+    print("massTable[tableIndex] is: " + str(massTable[tableIndex]))
+
+    tableIndex = tableIndex + 1
+    currentTime = initialTime
+    dragVelocity = initialVelocity
+    dragVelocityDataList.clear()
+    losslessVelocityDataList.clear()
+    timeDataList.clear()
+
+
+plt.legend(loc="best")
 plt.xlabel("Time (sec)")
 plt.ylabel("Velocity (m/s)")
 plt.grid(True)
-plt.plot(timeDataList, losslessVelocityDataList, label="without drag")
-plt.plot(timeDataList, dragVelocityDataList, label="with drag")
-plt.legend(loc="best")
 plt.show()
+
+
